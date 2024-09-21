3619138e5787dbe801a028e39cbd1f55f48cae24
bigliuliu <1552000425@qq.com>
2024-09-21 22:32:41 +0800

    added chat style and result display animation

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,37 +1,64 @@
 import { createContext, useState } from "react";
 import run from "../config/gemini";
 
-export  const Context = createContext()
+export const Context = createContext();
 
-const ContextProvider = (props)=>{
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-    const [input,setInput] = useState("")
-    const [recentPrompts,setRecentPrompts] =  useState("")
-    const [prevPrompts,setPrevPrompts] = useState([])
-    const [showResult, setShowResult] = useState(false)
-    const [loading,setLoading] = useState(false)
-    const [resultData,setResultData] = useState("")
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
 
-    const onSent = async (prompt) =>{
-      await  run(input)
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    setPrevPrompts((prev) => [...prev, input]);
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
     }
-    const contextValue = {
-        prevPrompts,
-        setPrevPrompts,
-        input,
-        setInput,
-        recentPrompts,
-        setRecentPrompts,
-        showResult,
-        setShowResult,
-        loading,
-        setLoading,
-        resultData,
-        setResultData,
-        onSent
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
     }
-    return (
-        <Context.Provider value={contextValue}> {props.children}</Context.Provider>
-    )
-}
-export default ContextProvider
+    setLoading(false);
+    setInput("");
+  };
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData,
+    onSent,
+  };
+  return (
+    <Context.Provider value={contextValue}> {props.children}</Context.Provider>
+  );
+};
+export default ContextProvider;
